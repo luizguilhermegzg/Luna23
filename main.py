a745f6bce52eda7496fe6ab8ba99745a6860907c
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-07 20:15:39 -0300

    alternative method to access the dicionaries with a .json file add

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-text = input("DIGITE O TEXTO: ")
+import json
+with open('encrypt-decrypt.json', 'r') as arq:
+    encrypt = json.load(arq)['encrypt']
+   # decrypt = json.load(arq)['decrypt']
 
+text = input("DIGITE O TEXTO: ")
 mode = print("[1] Encrypt\n[2] Decrypt\n"), input("SELECIONE O MODO: ")
 
-
-if mode[1] == '1' or mode[1] == '2':
+if mode[1] == '1' or mode[1] == '2' and text != "":
     if mode[1] == '1':
         counter = 0
         while counter < len(text):
-            text = text.replace(text[counter], keys_encrypt[text[counter]])
+            text = text.replace(text[counter], encrypt[text[counter]])
             counter = counter + 1
     if mode[1] == '2':
         counter = 0
         while counter < len(text):
-            text = text.replace(text[counter], keys_decrypt[text[counter]])
+            text = text.replace(text[counter], decrypt[text[counter]])
             counter = counter + 1
+    print("OUTPUT: "+text)
 else: 
-    print("Erro: nenhum modo disponivel selecionado")  
+    print("Erro: você não selecionou nenhum modo ou não digitou nenhum texto!!!")  
 
 
 
-print("OUTPUT: "+text)
+
 
 

0837e5ab350aad8f1ee8a4ec849e25b63743723e
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-08 15:56:19 -0300

    finale finished program, but need to add some characters to > encrypt-decrypt.json

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import json
-with open('encrypt-decrypt.json', 'r') as arq:
-    encrypt = json.load(arq)['encrypt']
-   # decrypt = json.load(arq)['decrypt']
-
+with open('encrypt-decrypt.json', 'r') as arq_encrypt:
+    encrypt = json.load(arq_encrypt)['encrypt']
+with open('encrypt-decrypt.json', 'r') as arq_decrypt:
+    decrypt = json.load(arq_decrypt)['decrypt']
 text = input("DIGITE O TEXTO: ")
 mode = print("[1] Encrypt\n[2] Decrypt\n"), input("SELECIONE O MODO: ")
 
 if mode[1] == '1' or mode[1] == '2' and text != "":
+    counter = 0
+    output_text = ""
     if mode[1] == '1':
-        counter = 0
         while counter < len(text):
-            text = text.replace(text[counter], encrypt[text[counter]])
+            output_text = output_text + encrypt[text[counter]]
             counter = counter + 1
     if mode[1] == '2':
-        counter = 0
         while counter < len(text):
-            text = text.replace(text[counter], decrypt[text[counter]])
+            output_text = output_text + decrypt[text[counter]]
             counter = counter + 1
-    print("OUTPUT: "+text)
+    print("OUTPUT: "+output_text)
 else: 
     print("Erro: você não selecionou nenhum modo ou não digitou nenhum texto!!!")  
 

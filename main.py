a87c0a58c9edf8e100797c627197c7b326e2a93f
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-10 21:16:17 -0300

    main.py >> modified

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import json
+# Import a json file with all characters replacements.
 with open('./json/encrypt-decrypt.json', 'r') as arq_encrypt:
     encrypt = json.load(arq_encrypt)['encrypt']
 with open('./json/encrypt-decrypt.json', 'r') as arq_decrypt:
     decrypt = json.load(arq_decrypt)['decrypt']
-text = input("\nDIGITE O TEXTO: ")
-mode = print("[1] Encrypt\n[2] Decrypt\n"), input("SELECIONE O MODO: ")
 
-if mode[1] == '1' or mode[1] == '2' and text != "":
-    counter = 0
-    output_text = ""
-    if mode[1] == '1':
-        while counter < len(text):
-            output_text = output_text + encrypt[text[counter]]
-            counter = counter + 1
-    if mode[1] == '2':
-        while counter < len(text):
-            output_text = output_text + decrypt[text[counter]]
-            counter = counter + 1
-    print("OUTPUT: "+output_text)
-else: 
-    print("Erro: você não selecionou nenhum modo ou não digitou nenhum texto!!!")  
+# Ask to input an text and mode of the replacement.
+def settings():
+    text = input("\nDIGITE O TEXTO: ")
+    mode = print("[1] Encrypt\n[2] Decrypt\n"), input("SELECIONE O MODO: ")
+    encrypt_decrypt(text, mode)
+
+# Finally here the text inserted is replaced by the characters of luna23.
+def encrypt_decrypt(inserted, mode_select): 
+    if mode_select[1] == '1' or mode_select[1] == '2' and inserted != "":
+        i = 0
+        output_text = ""
+        if mode_select[1] == '1':
+            while i < len(inserted):
+                output_text = output_text + encrypt[inserted[i]]
+                i += 1
+        if mode_select[1] == '2':
+            while i < len(inserted):
+                output_text = output_text + decrypt[inserted[i]]
+                i += 1
+        print("OUTPUT: "+output_text)
+    else: 
+        print("Erro: você não selecionou nenhum modo ou não digitou nenhum texto!!!")
+
+# Execute the initial function settings after encrypt_decrypt() function be defined.
+settings()
+
